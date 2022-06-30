b97b48e58eadf767cd2f082457006458e4ab3569
Haile2723 <haile10192019@gmail.com>
2022-06-30 04:15:22 -0700

    Task 5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -3,8 +3,8 @@
 # Haile2723
 
 if __name__ == "__main__":
-"""Print the value of variable a from variable_load_5."""
+    """Print the value of variable a from variable_load_5."""
+    
     from variable_load_5 import a
-
 
     print(a)
