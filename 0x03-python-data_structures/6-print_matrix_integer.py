a664c66bcff397db577affda2e23c4a66904d19a
Haile2723 <haile10192019@gmail.com>
2022-07-04 02:34:15 -0700

    Task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 6-print_matrix_integer.py
+# Haile2723
+
+
+
+def print_matrix_integer(matrix=[[]]):
+    """Print a matrix of integer"""
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i] - 1):
+                    print(" ", end="")
+
+        print(" ")
