23fc3491a06b79826227a01585ff69e3a514ff9d
Haile2723 <haile10192019@gmail.com>
2022-07-04 04:42:02 -0700

    Task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 9-max_integer.py
+# Haile2723
+
+
+
+def max_integer(my_list=[]):
+
+    if len(my_lis) == 0:
+        return (None)
+    for i in sorted(my_list):
+        return (i[0])
